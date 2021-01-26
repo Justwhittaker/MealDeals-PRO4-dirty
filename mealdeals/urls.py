c04aed9b854d06ec95ed0336a6b063a66cbfb32c
Justin Whittaker <just.whittaker@gmail.com>
2021-01-26 20:05:04 +0000

    feat(mealdeals): add wsgi.py,asgi.py,urls.py

---
+++ mealdeals/urls.py
@@ -0,0 +1,14 @@
+""" mealdeals URL config """
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts', include('allauth.urls')),
+    path('', include('home.urls')),
+    path('subscribe/', include('subscribe.urls')),
+    path('profile/', include('client_profile.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
